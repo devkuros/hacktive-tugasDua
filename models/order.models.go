be75c25df82fb4a0ab43b62978f0536166ce327b
Anugrah <88880298+devkuros@users.noreply.github.com>
2022-03-23 14:08:42 +0700

    Update order.models.go

--- models/order.models.go
+++ models/order.models.go
@@ -9,7 +9,7 @@ import (
 type Order struct {
 	OrderId      uint       `gorm:"primaryKey"`
 	CostumerName string     `json:"costumer_name" gorm:"not null;type:varchar(50)"`
-	ItemId       []Item     `json:"item_id" gorm:"foreignKey:Id"`
+	ItemId       []Item     `json:"item_id" gorm:"foreignKey:ItemId"`
 	OrderedAt    *time.Time `json:"ordered_at"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
